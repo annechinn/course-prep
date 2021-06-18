a18e396814d8b820b40afbfc5fb64774c8900ae8
Anne Chinn <achinn@nwlink.com>
2021-06-18 08:45:09 -0700

    added nytimes-books-js lab

---
+++ labs/nytimes-books-js/final/data/books.js
@@ -0,0 +1,7 @@
+async function getBooks () {
+    
+    const response = await axios.get(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=5Vd8O8baGS3WEG1eQVAaS2mG6K0VyHH8`)
+    const books = response.data.results.books;
+    // console.log(`GET books`, books);
+    return books;
+};
